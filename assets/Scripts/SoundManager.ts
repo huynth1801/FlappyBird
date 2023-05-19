3700cc1f123065d01d389ce7a21875253f7bd8c6
huynth1801 <huuhuy1801@gmail.com>
2023-05-19 14:36:31 +0700

    format code with prettier

--- assets/Scripts/SoundManager.ts
+++ assets/Scripts/SoundManager.ts
@@ -1,46 +1,45 @@
-import { _decorator, Component, Node, AudioClip, AudioSource } from 'cc';
+import { _decorator, Component, Node, AudioClip, AudioSource } from "cc";
 const { ccclass, property } = _decorator;
 
 export enum SoundType {
-    Fly = 0,
-    Score,
-    Die,
-    Hit
+  Fly = 0,
+  Score,
+  Die,
+  Hit,
 }
 
-@ccclass('SoundManager')
+@ccclass("SoundManager")
 export class SoundManager extends Component {
-    @property(AudioSource)
-    public audioSource: AudioSource = null!
+  @property(AudioSource)
+  public audioSource: AudioSource = null!;
 
-    // Sound effect for bird action
-    @property({type: AudioClip})
-    private flySound: AudioClip = null;
+  // Sound effect for bird action
+  @property({ type: AudioClip })
+  private flySound: AudioClip = null;
 
-    @property({type: AudioClip})
-    private scoreSound: AudioClip = null;
+  @property({ type: AudioClip })
+  private scoreSound: AudioClip = null;
 
-    @property({type: AudioClip})
-    private hitSound: AudioClip = null;
+  @property({ type: AudioClip })
+  private hitSound: AudioClip = null;
 
-    @property({type: AudioClip})
-    private dieSound: AudioClip = null;
+  @property({ type: AudioClip })
+  private dieSound: AudioClip = null;
 
-    playSound(soundType: SoundType){
-        switch(soundType){
-            case SoundType.Fly:
-                this.audioSource.playOneShot(this.flySound, 1);
-                break;
-            case SoundType.Score:
-                this.audioSource.playOneShot(this.scoreSound, 1);
-                break;
-            case SoundType.Die:
-                this.audioSource.playOneShot(this.dieSound, 1);
-                break;
-            case SoundType.Hit:
-                this.audioSource.playOneShot(this.hitSound, 1);
-                break;
-        }
+  playSound(soundType: SoundType) {
+    switch (soundType) {
+      case SoundType.Fly:
+        this.audioSource.playOneShot(this.flySound, 1);
+        break;
+      case SoundType.Score:
+        this.audioSource.playOneShot(this.scoreSound, 1);
+        break;
+      case SoundType.Die:
+        this.audioSource.playOneShot(this.dieSound, 1);
+        break;
+      case SoundType.Hit:
+        this.audioSource.playOneShot(this.hitSound, 1);
+        break;
     }
+  }
 }
-
--- assets/Scripts/Store.ts
+++ assets/Scripts/Store.ts
@@ -1,55 +1,55 @@
-import { _decorator, CCInteger, Component, Node, CCBoolean } from 'cc';
+import { _decorator, CCInteger, Component, Node, CCBoolean } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('Store')
+@ccclass("Store")
 export class Store extends Component {
-    @property({
-        type: CCInteger
-    })
-    private stored: Number = new Number(1)
+  @property({
+    type: CCInteger,
+  })
+  private stored: Number = new Number(1);
 
-    @property({
-        type : CCBoolean
-    })
-    private yellowBird: Boolean = true
-    @property({
-        type : CCBoolean
-    })
-    private blueBird: Boolean = false
-    @property({
-        type : CCBoolean
-    })
-    private redBird: Boolean = false
+  @property({
+    type: CCBoolean,
+  })
+  private yellowBird: Boolean = true;
+  @property({
+    type: CCBoolean,
+  })
+  private blueBird: Boolean = false;
+  @property({
+    type: CCBoolean,
+  })
+  private redBird: Boolean = false;
 
-    getValue() {
-        return this.stored
-    }
+  getValue() {
+    return this.stored;
+  }
 
-    setValue(newValue: number) {
-        this.stored = new Number(newValue);
-    }
+  setValue(newValue: number) {
+    this.stored = new Number(newValue);
+  }
 
-    getYellow() {
-        return this.yellowBird
-    }
+  getYellow() {
+    return this.yellowBird;
+  }
 
-    setYellow(yellow: boolean) {
-        this.yellowBird = new Boolean(yellow)
-    }
+  setYellow(yellow: boolean) {
+    this.yellowBird = new Boolean(yellow);
+  }
 
-    getRed() {
-        return this.redBird
-    }
+  getRed() {
+    return this.redBird;
+  }
 
-    setRed(red: boolean) {
-        this.redBird = new Boolean(red)
-    }
+  setRed(red: boolean) {
+    this.redBird = new Boolean(red);
+  }
 
-    getBlue() {
-        return this.blueBird
-    }
+  getBlue() {
+    return this.blueBird;
+  }
 
-    setBlue(blue: boolean) {
-        this.blueBird = new Boolean(blue)
-    }
+  setBlue(blue: boolean) {
+    this.blueBird = new Boolean(blue);
+  }
 }
